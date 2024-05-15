d2744682da21c6461289040ecfb51d18fa9581ad
vpinedo <74575048+vpinedo07@users.noreply.github.com>
2024-05-14 18:11:54 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -11,7 +11,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://3.11.80.124/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "http://3.11.80.124/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
